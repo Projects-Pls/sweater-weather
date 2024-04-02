7dedb352994eb774b490fb62413f100719d00080
hujeffy1 <hujeffy.1@gmail.com>
2024-04-02 14:28:25 -0400

    started making frontend components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
